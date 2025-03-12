bfde94d1d3ea0b9529ac14eee9a14e71914a76f6
Rui Barbosa <barduinor@gmail.com>
2025-03-12 16:08:46 -0400

    initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from box_sdk_gen import BoxClient
+from src.box_ai_agents_toolkit.box_authentication import (
+    get_ccg_client,
+    get_oauth_client,
+)
+
+
+@pytest.fixture
+def box_client_auth() -> BoxClient:
+    return get_oauth_client()
+
+
+@pytest.fixture
+def box_client_ccg() -> BoxClient:
+    return get_ccg_client()
