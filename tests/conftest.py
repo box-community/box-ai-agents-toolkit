b008bc73c32caa96f7cd9b151a62aaf8c816c979
Rui Barbosa <barduinor@gmail.com>
2025-03-24 17:46:02 -0400

    CI/CD script

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,13 @@
 import pytest
-from dotenv import load_dotenv
+
+# from dotenv import load_dotenv
 from box_sdk_gen import BoxClient
 from src.box_ai_agents_toolkit import (
     get_ccg_client,
     # get_oauth_client,
 )
 
-load_dotenv()
+# load_dotenv()
 
 # @pytest.fixture
 # def box_client_auth() -> BoxClient:
