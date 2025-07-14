7ec26837e75fd7a820645a4b345f02a3592661a6
Rui Barbosa <barduinor@gmail.com>
2025-07-14 17:57:44 -0400

    fixing exports

--- src/box_ai_agents_toolkit/__init__.py
+++ src/box_ai_agents_toolkit/__init__.py
@@ -1,191 +1,142 @@
 from box_sdk_gen import (
-    BoxClient,  # noqa: F401
-    BoxSDKError,  # noqa: F401
-    File,  # noqa: F401
-    Folder,  # noqa: F401
-    SearchForContentContentTypes,  # noqa: F401
+    BoxClient,
+    BoxSDKError,
+    File,
+    Folder,
+    SearchForContentContentTypes,
 )
 
-sdk = ["BoxClient", "BoxSDKError", "File", "Folder", "SearchForContentContentTypes"]
 
-
-# from box_ai_agents_toolkit.box_api_ai_deprecated import (  # noqa: E402
-#     box_claude_ai_agent_ask,  # noqa: F401
-#     box_claude_ai_agent_extract,  # noqa: F401
-#     box_file_ai_ask,  # noqa: F401
-#     box_file_ai_extract,  # noqa: F401
-#     box_file_ai_extract_structured,  # noqa: F401
-#     box_folder_ai_ask,  # noqa: F401
-#     box_folder_ai_extract,  # noqa: F401
-#     box_folder_ai_extract_structured,  # noqa: F401
-#     box_hubs_ai_ask,  # noqa: F401
-#     box_multi_file_ai_ask,  # noqa: F401
-#     box_multi_file_ai_extract,  # noqa: F401
-#     box_multi_file_ai_extract_structured,  # noqa: F401
-# )
-
-# ai_deprecated = [
-#     "box_file_ai_ask",
-#     "box_file_ai_extract",
-#     "box_file_ai_extract_structured",
-#     "box_claude_ai_agent_ask",
-#     "box_claude_ai_agent_extract",
-#     "box_folder_ai_ask",
-#     "box_folder_ai_extract",
-#     "box_folder_ai_extract_structured",
-#     "box_hubs_ai_ask",
-#     "box_multi_file_ai_ask",
-#     "box_multi_file_ai_extract",
-#     "box_multi_file_ai_extract_structured",
-# ]
-
-from box_ai_agents_toolkit.box_api_ai import (  # noqa: E402
-    box_ai_ask_file_multi,  # noqa: F401
-    box_ai_ask_file_single,  # noqa: F401
-    box_ai_ask_hub,  # noqa: F401
-    box_ai_extract_freeform,  # noqa: F401
-    box_ai_extract_structured_enhanced_using_fields,  # noqa: F401
-    box_ai_extract_structured_enhanced_using_template,  # noqa: F401
-    box_ai_extract_structured_using_fields,  # noqa: F401
-    box_ai_extract_structured_using_template,  # noqa: F401
+from box_ai_agents_toolkit.box_api_ai import (
+    box_ai_ask_file_multi,
+    box_ai_ask_file_single,
+    box_ai_ask_hub,
+    box_ai_extract_freeform,
+    box_ai_extract_structured_enhanced_using_fields,
+    box_ai_extract_structured_enhanced_using_template,
+    box_ai_extract_structured_using_fields,
+    box_ai_extract_structured_using_template,
 )
 
-ai = [
-    "box_ai_ask_file_single",
+
+from box_ai_agents_toolkit.box_api_file import (
+    box_file_download,
+    box_file_get_by_id,
+    box_file_text_extract,
+    box_upload_file,
+)
+
+
+from box_ai_agents_toolkit.box_api_folder import (
+    box_create_folder,
+    box_delete_folder,
+    box_folder_list_content,
+    box_update_folder,
+)
+
+
+from box_ai_agents_toolkit.box_api_search import (
+    box_locate_folder_by_name,
+    box_search,
+)
+
+
+from box_ai_agents_toolkit.box_api_util_classes import (
+    BoxFileExtended,
+    DocumentFiles,
+    ImageFiles,
+)
+
+
+from box_ai_agents_toolkit.box_authentication import (
+    authorize_app,
+    get_auth_config,
+    get_ccg_client,
+    get_ccg_config,
+    get_oauth_client,
+)
+
+
+from .box_api_docgen import (
+    box_docgen_create_batch,
+    box_docgen_create_batch_from_user_input,
+    box_docgen_get_job_by_id,
+    box_docgen_list_jobs,
+    box_docgen_list_jobs_by_batch,
+)
+
+
+from .box_api_docgen_template import (
+    box_docgen_template_create,
+    box_docgen_template_delete,
+    box_docgen_template_get_by_id,
+    box_docgen_template_list,
+    box_docgen_template_list_jobs,
+    box_docgen_template_list_tags,
+)
+
+
+from .box_api_metadata_template import (
+    box_metadata_delete_instance_on_file,
+    box_metadata_get_instance_on_file,
+    box_metadata_set_instance_on_file,
+    box_metadata_template_create,
+    box_metadata_template_get_by_id,
+    box_metadata_template_get_by_key,
+    box_metadata_template_get_by_name,
+    box_metadata_update_instance_on_file,
+)
+
+
+__all__ = [
+    "BoxClient",
+    "BoxSDKError",
+    "File",
+    "Folder",
+    "SearchForContentContentTypes",
     "box_ai_ask_file_multi",
+    "box_ai_ask_file_single",
     "box_ai_ask_hub",
     "box_ai_extract_freeform",
-    "box_ai_extract_structured_using_fields",
-    "box_ai_extract_structured_using_template",
     "box_ai_extract_structured_enhanced_using_fields",
     "box_ai_extract_structured_enhanced_using_template",
-]
-
-
-from box_ai_agents_toolkit.box_api_file import (  # noqa: E402
-    box_file_download,  # noqa: F401
-    box_file_get_by_id,  # noqa: F401
-    box_file_text_extract,  # noqa: F401
-    box_upload_file,  # noqa: F401
-)
-
-file = [
+    "box_ai_extract_structured_using_fields",
+    "box_ai_extract_structured_using_template",
     "box_file_download",
     "box_file_get_by_id",
     "box_file_text_extract",
     "box_upload_file",
-]
-
-from box_ai_agents_toolkit.box_api_folder import (  # noqa: E402
-    box_create_folder,  # noqa: F401
-    box_delete_folder,  # noqa: F401
-    box_folder_list_content,  # noqa: F401
-    box_update_folder,  # noqa: F401
-)
-
-folder = [
     "box_create_folder",
+    "box_delete_folder",
     "box_folder_list_content",
     "box_update_folder",
-    "box_delete_folder",
-]
-
-
-from box_ai_agents_toolkit.box_api_search import (  # noqa: E402
-    box_locate_folder_by_name,  # noqa: F401
-    box_search,  # noqa: F401
-)
-
-search = ["box_search", "box_locate_folder_by_name"]
-
-from box_ai_agents_toolkit.box_api_util_classes import (  # noqa: E402
-    BoxFileExtended,  # noqa: F401
-    DocumentFiles,  # noqa: F401
-    ImageFiles,  # noqa: F401
-)
-
-util_classes = ["ImageFiles", "BoxFileExtended", "DocumentFiles"]
-
-from box_ai_agents_toolkit.box_authentication import (  # noqa: E402
-    authorize_app,  # noqa: F401
-    get_auth_config,  # noqa: F401
-    get_ccg_client,  # noqa: F401
-    get_ccg_config,  # noqa: F401
-    get_oauth_client,  # noqa: F401
-)
-
-auth = [
-    "get_auth_config",
-    "get_ccg_config",
-    "get_ccg_client",
-    "get_oauth_client",
+    "box_locate_folder_by_name",
+    "box_search",
+    "BoxFileExtended",
+    "DocumentFiles",
+    "ImageFiles",
     "authorize_app",
-]
-
-from .box_api_docgen import (  # noqa: E402
-    box_docgen_create_batch,  # noqa: F401
-    box_docgen_create_batch_from_user_input,  # noqa: F401
-    box_docgen_get_job_by_id,  # noqa: F401
-    box_docgen_list_jobs,  # noqa: F401
-    box_docgen_list_jobs_by_batch,  # noqa: F401
-)
-
-docgen = [
+    "get_auth_config",
+    "get_ccg_client",
+    "get_ccg_config",
+    "get_oauth_client",
+    "box_docgen_create_batch",
+    "box_docgen_create_batch_from_user_input",
     "box_docgen_get_job_by_id",
     "box_docgen_list_jobs",
     "box_docgen_list_jobs_by_batch",
-    "box_docgen_create_batch",
-    "box_docgen_create_batch_from_user_input",
-]
-from .box_api_docgen_template import (  # noqa: E402
-    box_docgen_template_create,  # noqa: F401
-    box_docgen_template_delete,  # noqa: F401
-    box_docgen_template_get_by_id,  # noqa: F401
-    box_docgen_template_list,  # noqa: F401
-    box_docgen_template_list_jobs,  # noqa: F401
-    box_docgen_template_list_tags,  # noqa: F401
-)
-
-docgen_template = [
     "box_docgen_template_create",
-    "box_docgen_template_list",
     "box_docgen_template_delete",
     "box_docgen_template_get_by_id",
-    "box_docgen_template_list_tags",
+    "box_docgen_template_list",
     "box_docgen_template_list_jobs",
-]
-from .box_api_metadata_template import (  # noqa: E402
-    box_metadata_delete_instance_on_file,  # noqa: F401
-    box_metadata_get_instance_on_file,  # noqa: F401
-    box_metadata_set_instance_on_file,  # noqa: F401
-    box_metadata_template_create,  # noqa: F401
-    box_metadata_template_get_by_id,  # noqa: F401
-    box_metadata_template_get_by_key,  # noqa: F401
-    box_metadata_template_get_by_name,  # noqa: F401
-    box_metadata_update_instance_on_file,  # noqa: F401
-)
-
-metadata_template = [
-    "box_metadata_template_get_by_key",
-    "box_metadata_template_get_by_id",
-    "box_metadata_template_get_by_name",
-    "box_metadata_set_instance_on_file",
-    "box_metadata_get_instance_on_file",
+    "box_docgen_template_list_tags",
     "box_metadata_delete_instance_on_file",
-    "box_metadata_update_instance_on_file",
+    "box_metadata_get_instance_on_file",
+    "box_metadata_set_instance_on_file",
     "box_metadata_template_create",
+    "box_metadata_template_get_by_id",
+    "box_metadata_template_get_by_key",
+    "box_metadata_template_get_by_name",
+    "box_metadata_update_instance_on_file",
 ]
-
-
-__all__ = []
-__all__.extend(sdk)  # type: ignore
-# __all__.extend(ai_deprecated)  # type: ignore
-__all__.extend(ai)  # type: ignore
-__all__.extend(file)  # type: ignore
-__all__.extend(folder)  # type: ignore
-__all__.extend(search)  # type: ignore
-__all__.extend(auth)  # type: ignore
-__all__.extend(docgen)  # type: ignore
-__all__.extend(docgen_template)  # type: ignore
-__all__.extend(metadata_template)  # type: ignore
-__all__.extend(util_classes)  # type: ignore
